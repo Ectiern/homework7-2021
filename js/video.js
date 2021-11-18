5ae915d5f28166d0b90d62fb8d3d9887ad696c64
Ectiern <78368442+Ectiern@users.noreply.github.com>
2021-11-18 15:13:50 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -4,8 +4,6 @@ var volume = document.querySelector("#volume");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-	video=document.querySelector("#player1");
-	volume - document.querySelector("#volume");
 
 });
 
